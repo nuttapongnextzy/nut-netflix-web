2a24d65e3bf226bdaecf1222ec917886f12c679d
Nuttapong Phisitbutr <nuttapong.p@nextzy.com>
2025-06-11 15:25:44 +0700

    fix: adjust Search page layout

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,66 +1,54 @@
 import React from "react"
-import { Movie } from "@/lib/definitions";
+import { fetchMovies } from '@/lib/data';
 import Link from "next/link";
 import Image from "next/image";
+import { MagnifyingGlassIcon, MicrophoneIcon } from '@heroicons/react/24/solid';
+import { PlayCircleIcon } from '@heroicons/react/24/outline';
+import { randomNum } from "@/lib/utils/number-utils";
 
-const movies: Movie[] = [
-  {
-    id: '1',
-    title: 'Spider Man',
-    posterUrl: '/movie-poster-1.svg',
-  },
-  {
-    id: '1',
-    title: 'Star Wars',
-    posterUrl: '/movie-poster-1.svg',
-  },
-  {
-    id: '1',
-    title: 'The Matrix',
-    posterUrl: '/movie-poster-1.svg',
-  },
-  {
-    id: '1',
-    title: 'Titanic',
-    posterUrl: '/movie-poster-1.svg',
-  },
-  {
-    id: '1',
-    title: 'Squid Game',
-    posterUrl: '/movie-poster-1.svg',
-  },
-];
+const page = async () => {
+  const [recommendedMovies] = await Promise.all([
+    fetchMovies('popular', randomNum(5, 10)),
+  ]);
 
-const page = () => {
   return (
     <div className="flex flex-col bg-black min-h-screen">
-      <div className="mt-10">
+      <div className="relative mt-10">
+        <MagnifyingGlassIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-white/70 w-6 h-6" />
+
+        <Link
+          href='#'
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-white/70 z-10"
+        >
+          <MicrophoneIcon className="w-6 h-6" />
+        </Link>
+
         <input
-          type="email"
-          name="email"
-          id="email"
+          type="search"
+          name="search"
+          id="search"
           placeholder="Search anime, movies, shows.."
-          className="w-full h-12 bg-white/20 opacity-90 px-3 py-1.5 outline-1 rounded-[1px] text-white placeholder:text-white/50 placeholder:text-xs placeholder:font-normal">
+          className="px-10 w-full h-12 bg-white/20 opacity-90 py-1.5 outline-1 rounded-[1px] text-white placeholder:text-white/50 placeholder:text-xs placeholder:font-normal">
         </input>
       </div>
 
       <h1 className="text-white font-bold mt-3 p-2">Recommended Animes & Movies</h1>
 
-      <div className="mt-1 p-2">
-        {movies.map((movie, index) => (
+      <div className="flex flex-col p-1">
+        {recommendedMovies.map((movie, index) => (
           <div
             key={index}
-            className=""
+            className="flex flex-col justify-between m-1"
           >
             <Link
-              href="/detail/1"
+              href={`/detail/${movie.id}`}
               className="cursor-pointer"
             >
-              <div className="flex justify-between items-center m-2">
+              <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-1">
                   <Image
-                    src={movie.posterUrl}
-                    alt="Description of image"
+                    src={movie.backdropUrl ?? movie.posterUrl}
+                    alt="Movie image"
                     width={100}
                     height={150}
                     className=""
@@ -70,9 +58,7 @@ const page = () => {
                   </div>
                 </div>
 
-                <div className="text-white">
-                  ▶
-                </div>
+                <PlayCircleIcon className="w-6 h-6" />
               </div>
 
             </Link>
